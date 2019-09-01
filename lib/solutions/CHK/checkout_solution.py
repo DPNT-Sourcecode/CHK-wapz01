8e6511c0fb010e217ac50e35f54c2c07029aa14f
sbx_user1051 <sbx_user1051@169.254.42.5>
2019-09-01 13:37:29 +0000

    Sun Sep 01 13:37:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -16,18 +16,18 @@ def checkout(skus):
     for x in skus:
         if x in allowed_skus:
             sku_map = {x: skus.count(x)}
+        else:
+            return -1
             
-
-
     total_checkout = 0
     for sku, num in sku_map.items():
-        # check for discounts.
+        
         total_checkout = num * price_for_sku[sku]
 
     return total_checkout
     
 
-print(checkout(""))
+print(checkout("AxA"))
 # - {"method":"checkout","params":[""],"id":"CHK_R1_002"}, expected: 0, got: -1
 # - {"method":"checkout","params":["ABCa"],"id":"CHK_R1_009"}, expected: -1, got: 20
 # - {"method":"checkout","params":["AxA"],"id":"CHK_R1_010"}, expected: -1, got: 100

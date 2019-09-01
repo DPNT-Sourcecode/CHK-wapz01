4632f88d7f1fbc6a677bde6fe40f9449d79f4d6f
sbx_user1051 <sbx_user1051@169.254.42.5>
2019-09-01 13:31:29 +0000

    Sun Sep 01 13:31:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,7 +3,26 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    return skus
+    allowed_skus = "ABCD"
 
+    price_for_sku = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
 
-print(checkout("2B"))
+    sku_map = {}
+    for x in skus:
+        if x in allowed_skus:
+            sku_map = {x: skus.count(x)}
+            
+    if len(sku_map) == 0:
+        return -1
+
+    total_checkout = 0
+    for sku, num in sku_map.items():
+        # check for discounts.
+        total_checkout = num * price_for_sku[sku]
+
+    return total_checkout

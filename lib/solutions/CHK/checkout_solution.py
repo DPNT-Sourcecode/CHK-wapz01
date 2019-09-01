66db73e9875acd9b10b05f0972cd23e5111beaf0
sbx_user1051 <sbx_user1051@169.254.42.5>
2019-09-01 13:40:29 +0000

    Sun Sep 01 13:40:29 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,12 +22,19 @@ def checkout(skus):
     total_checkout = 0
     for sku, num in sku_map.items():
         
-        total_checkout = num * price_for_sku[sku]
+        # discounts
+        if sku == "A" and num == 3:
+            total_checkout = num * 130
+        
+        if sku == "B" and num == 3:
+            total_checkout = num * 130
+        
+        #total_checkout = num * price_for_sku[sku]
 
     return total_checkout
     
 
-print(checkout("AxA"))
+print(checkout("AAA"))
 # - {"method":"checkout","params":[""],"id":"CHK_R1_002"}, expected: 0, got: -1
 # - {"method":"checkout","params":["ABCa"],"id":"CHK_R1_009"}, expected: -1, got: 20
 # - {"method":"checkout","params":["AxA"],"id":"CHK_R1_010"}, expected: -1, got: 100
